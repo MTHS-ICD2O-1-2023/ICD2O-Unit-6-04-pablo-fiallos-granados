91192076d476d00a44c0ee98925da4aad247cf43
Pablo Fiallos <159185130+pablo-fiallos-granados@users.noreply.github.com>
2024-06-02 18:09:55 +0000

    everything

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Pablo Fiallos
+// Created on: June 2024
 // This file contains the JS functions for index.html
+
+"use strict"
+
+window.onload = function () {
+  const params = new URLSearchParams(document.location.search)
+
+  // input
+  const radius = params.get("radius")
+  console.log(radius)
+
+  // process
+  const volume = (4 / 3) * Math.PI * radius ** 2
+
+  // output
+  document.getElementById("volume-info").innerHTML =
+    "Volume = " + volume + "u<sup>3</sup>"
+}
